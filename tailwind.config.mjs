771ae66130d8944b161e6b86e0d41352710d8c36
leonelvalentinn <leonelvalentinn@gmail.com>
2024-02-13 15:26:38 -0600

    New Banners

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,11 @@ export default {
 				'gold-200': 'rgb(140,92,36)',
 				'gold-100': 'rgb(219,182,115)',
       },
+			gridTemplateColumns: {
+        // Simple 16 column grid
+        'promo': 'repeat(2, minmax(0, 350px))',
+				'promo2': 'repeat(4, minmax(0, 350px))',
+      }
 		},
 	},
 	plugins: [],
